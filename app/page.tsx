68fceed2e19e18422bfc713af8057ebdfd7a2a28
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-10-28 13:19:27 +0900

    feat: add link to login, signup page in root

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import HomeHeader from "@/components/HomeHeader";
 import style from "@/styles/index.module.scss";
 
@@ -5,6 +6,15 @@ export default function Home() {
   return (
     <main className={style.main}>
       <HomeHeader />
+      <section className={style.content}>
+        <h2 className={style.h2}>스스로를 돌아보는 시간</h2>
+        <Link href="/login" className={style.toLogin}>
+          로그인
+        </Link>
+        <Link href="/signup" className={style.toSignup}>
+          회원가입
+        </Link>
+      </section>
     </main>
   );
 }
