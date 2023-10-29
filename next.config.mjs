f41e36b536033903b057eadf44409654619cfee8
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-10-29 11:16:32 +0900

    feat: update config
    Set default sass options.
    Remain rewrites for case to use it

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
-import config from "./config/index.mjs";
+// import config from "./config/index.mjs";
 
 const nextConfig = {
   sassOptions: {
     additionalData: `
+      @import "@/styles/variables.module.scss";
+      @import "@/styles/globals.module.scss";
     `,
   },
+  async rewrites() {
+    return [
+      // POST /login -> config.api/login
+      // { source: "/api/:path*", destination: `${config.api}/:path*` },
+    ];
   },
 };
 
