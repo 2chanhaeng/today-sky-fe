c6646abfd89a22db352f19de370164797d54f363
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-10-29 10:37:57 +0900

    feat: get path by date

--- app/api/diary/[year]/[month]/[date]/route.ts
+++ app/api/diary/[year]/[month]/[date]/route.ts
@@ -3,15 +3,15 @@
 import { NextRequest, NextResponse } from "next/server";
 import { DateParams } from "@/types/params";
 import { PostDiary } from "@/types/request";
+import getPathByDate from "@/utils/getPathByDate";
 import getAccess from "@/utils/getAccess";
 import paramsToObject from "@/utils/paramsToObject";
 import reqData from "@/utils/reqData";
 
-export async function POST(
-  req: NextRequest,
-  { params: { year, month, date } }: DateParams
-) {
-  const path = `/diary/${year}/${month}/${date}`;
+const getPath = getPathByDate("diary");
+
+export async function POST(req: NextRequest, params: DateParams) {
+  const path = getPath(params);
   const diary = paramsToObject<PostDiary>(await req.text());
   await reqData<PostDiary, boolean>(path)(await getAccess())("POST")(diary);
   return NextResponse.redirect(
