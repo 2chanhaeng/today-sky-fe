4d9a3ce7b505e023c8e2cf786e277a1a9ca66930
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-11-07 20:27:49 +0900

    feat: process signup as server action

--- components/SignupForm/actions.ts
+++ components/SignupForm/actions.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import { Userinfo } from "@/types/login";
+import formToObj from "@/utils/formToJson";
 import requestData from "@/utils/requestData";
+import { redirect } from "next/navigation";
 
 export async function checkUsernameDupl(username: string) {
   try {
@@ -11,5 +14,20 @@ export async function checkUsernameDupl(username: string) {
   } catch (err) {
     console.log(err);
     return undefined;
+  }
+}
+
+export async function signup(formdata: FormData) {
+  try {
+    const userinfo = formToObj<Userinfo>(formdata);
+    const res = await requestData<Userinfo, boolean>("POST")(
+      "/signup",
+      userinfo
+    );
+    if (!res) throw new Error("signup failed");
+    return redirect(`/login`);
+  } catch (e) {
+    console.log(e);
+    return redirect(`/signup?error=failed`);
   }
 }
--- components/SignupForm/index.tsx
+++ components/SignupForm/index.tsx
@@ -1,9 +1,10 @@
 import UserInfo from "./UserInfo";
+import { signup } from "./actions";
 import style from "./style.module.scss";
 
 export default function SignupForm() {
   return (
-    <form className={style.form} method="POST" action="/api/signup">
+    <form className={style.form} method="POST" action={signup}>
       <UserInfo />
       <button className={style.toSignup} type="submit">
         회원가입
