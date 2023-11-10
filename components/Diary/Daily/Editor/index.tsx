ab0c408951bfd17285c3ea43381a1cd87d69ae45
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-11-10 19:13:29 +0900

    feat: use transition

--- components/Diary/Daily/Editor/index.tsx
+++ components/Diary/Daily/Editor/index.tsx
@@ -1,9 +1,13 @@
+"use client";
+
+import { useTransition } from "react";
 import Textarea from "./Textarea";
 import { postDiary } from "./actions";
 import style from "../style.module.scss";
 
 export default function Editor({ prev, path }: { prev: string; path: string }) {
   const post = postDiary.bind(null, path);
+  const [isPending, startTransition] = useTransition();
   return (
     <form className={style.editor} action={post}>
       <Textarea name="content" defaultValue={prev} rows={4} />
