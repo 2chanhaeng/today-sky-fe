9658e6967cb14e96e7abf13372b55709e8c8fb77
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-11-10 19:07:40 +0900

    feat: use value state

--- components/Diary/Daily/Editor/Textarea.tsx
+++ components/Diary/Daily/Editor/Textarea.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 
 interface ElemProps {
   name: string;
@@ -12,16 +12,20 @@ interface ActionProps {
 interface Props extends ElemProps, ActionProps {}
 
 export default function Textarea({ post, ...elemProps }: Props) {
+  const [value, setValue] = useState(elemProps.defaultValue || "");
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     // textarea auto resize
     e.target.style.height = "auto";
     e.target.style.height = e.target.scrollHeight + "px";
 
+    setValue(e.target.value);
   };
   return (
     <textarea
       {...elemProps}
+      value={value}
       onChange={handleChange}
+      rows={Math.max(value.split("\n").length, 5)}
     ></textarea>
   );
 }
