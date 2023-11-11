354701a634e436f29d2777d463ba9da673018c52
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-11-12 08:44:15 +0900

    refac: separate days(dates) component
    Separate dates component and rename to days.

--- components/Calendar/index.tsx
+++ components/Calendar/index.tsx
@@ -1,8 +1,8 @@
-import Link from "next/link";
+import { AppDates } from "@/types/params";
 import { Header } from "./Header";
 import { Weekdays } from "./Weekdays";
+import { Days } from "./Days";
 import style from "./style.module.scss";
-import { App, AppDates } from "@/types/params";
 
 export default function Calendar({ app, year, month }: AppDates) {
   return (
@@ -10,64 +10,12 @@ export default function Calendar({ app, year, month }: AppDates) {
       <summary className={style.calendarSummary}></summary>
       <Header app={app} year={year} month={month} />
       <Weekdays />
-      <Dates app={app} year={year} month={month} />
+      <Days app={app} year={year} month={month} />
       <Footer />
     </details>
   );
 }
 
-function Dates({ app, year, month }: AppDates) {
-  const first = new Date(year, month - 1, 1);
-  const last = new Date(year, month, 0);
-  const firstSun = new Date(
-    first.getFullYear(),
-    first.getMonth(),
-    1 - first.getDay()
-  );
-  const lastSat = new Date(
-    last.getFullYear(),
-    last.getMonth(),
-    last.getDate() + (6 - last.getDay())
-  );
-  const [fsy, fsm, fsd] = getYMD(firstSun);
-  const dates = Array.from(
-    { length: (lastSat.getTime() - firstSun.getTime()) / 86400000 + 1 },
-    (_, i) => new Date(fsy, fsm - 1, fsd + i)
-  );
-  const DateDiv = createDateDiv(app!, month);
-  return (
-    <section className={style.dates}>
-      {dates.map((date, index) => (
-        <DateDiv date={date} key={index} />
-      ))}
-    </section>
-  );
-}
-
-function createDateDiv(app: App, basis: number) {
-  const isThisMonth = compareMonth(basis);
-  return function DateDiv({ date }: { date: Date }) {
-    const [year, month, day] = getYMD(date);
-    return (
-      <div className={`${style.date} ${isToday(date) && style.today}`}>
-        <Link
-          href={`/${app}/${year}/${month}/${day}`}
-          className={`${style.link} ${style[`${isThisMonth(month)}-month`]}`}
-        >
-          {day === 1 ? `${month}/${day}` : day}
-        </Link>
-      </div>
-    );
-  };
-}
-
 function Footer() {
   return <footer className={style.footer}></footer>;
-}
-
-function compareMonth(basis: number) {
-  return (month: number) => {
-    const diff = (month - basis) % 12;
-    return diff === 0 ? "this" : diff === 1 ? "next" : "prev";
-  };
 }
