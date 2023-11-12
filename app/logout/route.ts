9827b4af6fda705c7c3cefb5fbac1f2d35c2bdac
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-11-12 10:15:08 +0900

    refac: request data replace reqData when logout

--- app/logout/route.ts
+++ app/logout/route.ts
@@ -1,10 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import reqData from "@/utils/reqData";
-import getAccess from "@/utils/getAccess";
+import requestData from "@/utils/requestData";
 
 export async function GET({ nextUrl: { origin } }: NextRequest) {
-  await reqData<null, null>("/logout")(await getAccess())("GET")();
+  await requestData<null, null>("GET")("/logout");
   cookies().delete("access");
   cookies().delete("refresh");
   return NextResponse.redirect(`${origin}`, { status: 303 });
